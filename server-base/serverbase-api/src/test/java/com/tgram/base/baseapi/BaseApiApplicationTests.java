38bc3d3c946d6462be7a6d97c6af7a9bd3829c56
Dean <yelinsen05@163.com>
2019-11-19 16:25:49 +0800

    添加

---
+++ server-base/serverbase-api/src/test/java/com/tgram/base/baseapi/BaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgram.base.baseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
