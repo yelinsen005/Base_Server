34461d90fcc788ebffc2a149c38878f93a1ca157
Dean <yelinsen05@163.com>
2019-11-19 18:55:21 +0800

    swagger2

--- server-base/serverbase-demo/serverbase-demo-server/src/main/java/com/tgram/base/DemoServerApplication.java
+++ server-base/serverbase-demo/serverbase-demo-server/src/main/java/com/tgram/base/DemoServerApplication.java
@@ -1,42 +1,42 @@
-package com.tgram.base;
-
-import com.github.zuihou.user.annotation.EnableLoginArgResolver;
-import com.github.zuihou.validator.config.EnableFormValidator;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.Environment;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
-
-@SpringBootApplication
-@EnableDiscoveryClient
-@EnableHystrix
-@EnableFeignClients(value = {
-        "com.github.zuihou",
-})
-@EnableTransactionManagement
-@Slf4j
-@EnableLoginArgResolver
-@EnableFormValidator
-public class DemoServerApplication {
-    public static void main(String[] args) throws UnknownHostException {
-        ConfigurableApplicationContext application = SpringApplication.run(DemoServerApplication.class, args);
-        Environment env = application.getEnvironment();
-        log.info("\n----------------------------------------------------------\n\t" +
-                        "应用 '{}' 运行成功! 访问连接:\n\t" +
-                        "Swagger文档: \t\thttp://{}:{}/doc.html\n" +
-                        "----------------------------------------------------------",
-                env.getProperty("spring.application.name"),
-                InetAddress.getLocalHost().getHostAddress(),
-                env.getProperty("server.port"));
-
-    }
-}
+//package com.tgram.base;
+//
+//import com.github.zuihou.user.annotation.EnableLoginArgResolver;
+//import com.github.zuihou.validator.config.EnableFormValidator;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.context.ConfigurableApplicationContext;
+//import org.springframework.core.env.Environment;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+//
+//import java.net.InetAddress;
+//import java.net.UnknownHostException;
+//
+//
+//@SpringBootApplication
+//@EnableDiscoveryClient
+//@EnableHystrix
+//@EnableFeignClients(value = {
+//        "com.github.zuihou",
+//})
+//@EnableTransactionManagement
+//@Slf4j
+//@EnableLoginArgResolver
+//@EnableFormValidator
+//public class DemoServerApplication {
+//    public static void main(String[] args) throws UnknownHostException {
+//        ConfigurableApplicationContext application = SpringApplication.run(DemoServerApplication.class, args);
+//        Environment env = application.getEnvironment();
+//        log.info("\n----------------------------------------------------------\n\t" +
+//                        "应用 '{}' 运行成功! 访问连接:\n\t" +
+//                        "Swagger文档: \t\thttp://{}:{}/doc.html\n" +
+//                        "----------------------------------------------------------",
+//                env.getProperty("spring.application.name"),
+//                InetAddress.getLocalHost().getHostAddress(),
+//                env.getProperty("server.port"));
+//
+//    }
+//}
--- server-base/serverbase-swagger2/src/test/java/com/tgram/base/swagger2/serverbaseswagger2/ServerbaseSwagger2ApplicationTests.java
+++ server-base/serverbase-swagger2/src/test/java/com/tgram/base/swagger2/serverbaseswagger2/ServerbaseSwagger2ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tgram.base.swagger2starter.serverbaseswagger2;
+package com.tgram.base.swagger2.serverbaseswagger2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
